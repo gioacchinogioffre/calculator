a39bc920aba1cb513e30b68ba28b11a7eb8bdbbf
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-25 12:11:47 -0300

    fluent assertion test

--- Calculator/UnitTest1.cs
+++ Calculator/UnitTest1.cs
@@ -1,16 +1,25 @@
 ﻿namespace CalculatorTest;
 using Domain;
+using FluentAssertions;
 
-public class UnitTest1
+public class CalculatorTests
 {
     [Fact]
     public void Sum_of_2_and_2_should_be_4()
-    {
-        var calculator = new Calculator();
-        var result = calculator.Sum(2, 2);
 
-        if (result != 4) throw new Exception($"The Sum(2,2) was expected to be 4 but it's {result}.");
-    }
+    //{
+    //    var calculator = new Calculator();
+    //    var result = calculator.Sum(2, 2);
+
+    //    //if (result != 4) throw new Exception($"The Sum(2,2) was expected to be 4 but it's {result}.");
+
+    //    // FluentAssertions
+    //    result.Should().Be(4);
+    //}
+
+    => new Calculator()
+        .Sum(2, 2)
+        .Should().Be(4);
 
   
 }
--- Domain/Calculator.cs
+++ Domain/Calculator.cs
@@ -2,8 +2,6 @@
 public class Calculator
 {
     public int Sum(int left, int right)
-    {
-        return left + right;
-    }
+    => left + right;
 }
 
