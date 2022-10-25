39fa0b2fc41749d0dc1f7071c6f0edf29a635f2f
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-25 11:23:57 -0300

    unit test calculator

--- Calculator/UnitTest1.cs
+++ Calculator/UnitTest1.cs
@@ -1,10 +1,16 @@
-﻿namespace Calculator;
+﻿namespace CalculatorTest;
+using Domain;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        var calculator = new Calculator();
 
+        if (calculator.Sum(2,2) != 4)
+        throw new Exception();
     }
+
+  
 }
---
+++ Domain/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+public class Calculator
+{
+    public int Sum(int left, int right)
+    {
+        return left + right;
+    }
+}
+
